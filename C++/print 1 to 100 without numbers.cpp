13ec58d1993ce75cb52dc3478f3defaaa890c68a
Saloni <salonibindal81@gmail.com>
2022-10-13 17:32:39 +0530

    Added print 1 to 100 without numbers.cpp

---
+++ C++/print 1 to 100 without numbers.cpp
@@ -0,0 +1,15 @@
+//github username - binsal
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a; 
+    a -= a; 
+    for (a++; a <= 'a'; a++) { 
+    cout<< a; 
+   } 
+
+	return 0;
+}
